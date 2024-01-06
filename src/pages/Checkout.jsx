cd250b0534883e8844a63fd18dc48150cc019fb5
DeGlazed <raducutanase@gmail.com>
2024-01-06 19:00:59 +0200

    Fix typo

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -27,9 +27,9 @@ const Checkout = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-12 py-5 bg-light text-center">
-            <h4 className="p-3 display-5">Cosul dumneavoastra de cumparaturi este gol!</h4>
+            <h4 className="p-3 display-5">Coșul dumneavoastră de cumpărături este gol!</h4>
             <Link to="/mvp" className="btn btn-outline-dark mx-4">
-              <i className="fa fa-arrow-left"></i> Continuati cumparaturile!
+              <i className="fa fa-arrow-left"></i> Continuați cumpărăturile!
             </Link>
           </div>
         </div>
