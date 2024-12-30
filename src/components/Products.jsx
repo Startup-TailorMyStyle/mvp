65ca0ff6447bcaf513a059a1cc78d44430cc053a
Alexandra Ispas <aispas@adobe.com>
2024-12-30 17:32:13 +0200

    change image server

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -26,7 +26,7 @@ const Products = () => {
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
-      const response = await fetch("https://fakestoreapi.com/products/");
+      const response = await fetch("http://imageserver.local/products/");
       if (componentMounted) {
         setData(await response.clone().json());
         setFilter(await response.json());
